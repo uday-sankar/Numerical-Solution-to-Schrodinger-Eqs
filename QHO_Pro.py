54267aa70e1e4c2bc1f7893e7e3da20f829fa6b9
uday-sankar <47320836+uday-sankar@users.noreply.github.com>
2020-10-10 20:30:27 +0530

    Add files via upload

---
+++ QHO_Pro.py
@@ -0,0 +1,16 @@
+import Schro_Declutter as EF
+import numpy as np
+import matplotlib.pyplot as plt
+def Poten_H(x):
+	V=m*(w*x)**2/2
+	return V
+h,m,w=1,1,1
+EF.Constant_feeder(h,m,1,Poten_H)
+X=np.linspace(-8,8,10**4)
+E_eigen=EF.Eigen_Range_finder(X,0,5,2)
+fig=plt.figure()
+ax=plt.axes(xlabel='X',ylabel='Psi')
+ax.set_title("Solutions of quantum harmonic oscilator")
+EF.Plot_Eq(X,E_eigen,ax)
+ax.plot(X,Poten_H(X))
+plt.show()
