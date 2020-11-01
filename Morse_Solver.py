c378529090c8e614477f33c613d234eab0010acc
uday-sankar <47320836+uday-sankar@users.noreply.github.com>
2020-11-01 20:57:58 +0530

    Add files via upload

--- Morse_Solver.py
+++ Morse_Solver.py
@@ -7,27 +7,26 @@ def Morse_V(r):
 	return V
 
 m,h=1,1
-t='t'
 Xmin=.5
-Xmax=5
+Xmax=6
 D_e=10
 a=.8
 r_e=1
-X=np.linspace(Xmin,Xmax,10**3)
-for p in range(len(X)):
-	if Morse_V(X[p])<D_e:
-		Xmin=X[p]-1
+R=np.linspace(Xmin,Xmax,10**3)
+for p in range(len(R)):
+	if Morse_V(R[p])<D_e:
+		Xmin=R[p]-1
 		Pos=p
 		break
-X=np.linspace(Xmin,Xmax,10**4)
+R=np.linspace(Xmin,Xmax,10**4)
 EF.Constant_feeder(h,m,D_e,Morse_V)
 
-
-Eigen_E=EF.Eigen_Range_finder(X,0,D_e*.9,10,.1)
+Eigen_E=EF.Eigen_Range_finder(R,0,D_e*.9,10,.1)
 fig=plt.figure()
 ax=plt.axes(xlabel="r",ylabel="Psi",ylim=(0,Eigen_E[-1]*1.1))
-X=np.linspace(Xmin,Xmax,10**5)
-EF.Plot_Eq(X,Eigen_E,ax)
-ax.plot(X,Morse_V(X))
+R=np.linspace(Xmin,Xmax,10**5)
+EF.Plot_Eq(R,Eigen_E,ax)
+EF.Analatic_mult(R,np.arange(len(Eigen_E)),D_e,a,r_e,ax)
+ax.plot(R,Morse_V(R))
 plt.legend()
 plt.show()
